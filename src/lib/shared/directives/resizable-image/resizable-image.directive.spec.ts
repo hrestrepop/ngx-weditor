2d5f70a2c8f7796ce6770948d9289621e8328342
Harrisson Restrepo Parra <harrisson@iMac-de-Harrisson.local>
2020-05-11 16:27:56 +0100

    Documents update

--- src/lib/shared/directives/resizable-image/resizable-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ResizableImageDirective } from './resizable-image.directive';
-
-describe('ResizableImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResizableImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-weditor
- */
-
-export * from './lib/ngx-weditor.service';
-export * from './lib/ngx-weditor.component';
-export * from './lib/ngx-weditor.module';
